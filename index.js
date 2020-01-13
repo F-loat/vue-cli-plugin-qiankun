b84c942003a30f759de2dc9420c0f69334221b65
F-loat <chaimaoyuan@foxmail.com>
2020-01-13 13:38:27 +0800

    feat: auto config publicPath

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
-module.exports = (api) => {
-  const { name } = api.service.pkg
+module.exports = (api, projectOptions) => {
+  const { name, scripts } = api.service.pkg
+  const { publicPath, devServer } = projectOptions
+
+  // 自动注入 publicPath 配置
+  if (
+    process.env.NODE_ENV !== 'production' &&
+    publicPath === '/'
+  ) {
+    // eslint-disable-next-line camelcase
+    const { npm_lifecycle_event, PORT } = process.env
+
+    const currentScript = scripts[npm_lifecycle_event]
+
+    const { port } = require('minimist')(currentScript.split(' '))
+
+    const realPort = port || PORT || devServer.port
+
+    if (realPort) {
+      projectOptions.publicPath = `//localhost:${realPort}/`
+    }
+  }
 
   api.configureWebpack(() => {
     return {
