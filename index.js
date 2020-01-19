3adb2db00605268631fca445dab29213435bd864
F-loat <chaimaoyuan@foxmail.com>
2020-01-19 13:48:56 +0800

    feat: use chainWebpack

--- index.js
+++ index.js
@@ -21,20 +21,15 @@ module.exports = (api, projectOptions) => {
     }
   }
 
-  api.configureWebpack(() => {
-    return {
-      devServer: {
-        // 允许跨域
-        headers: {
-          'Access-Control-Allow-Origin': '*'
-        }
-      },
-      output: {
-        // 把应用打包成 umd 库格式
-        library: `${name}-[name]`,
-        libraryTarget: 'umd',
-        jsonpFunction: `webpackJsonp_${name}`
-      }
-    }
+  api.chainWebpack((config) => {
+    config.devServer
+      .headers({
+        'Access-Control-Allow-Origin': '*'
+      })
+
+    config.output
+      .library(`${name}-[name]`)
+      .libraryTarget('umd')
+      .jsonpFunction(`webpackJsonp_${name}`)
   })
 }
