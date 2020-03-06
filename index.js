c139adc4f1a2bbc42f741b72be696bf32e1aeb92
F-loat <chaimaoyuan@foxmail.com>
2020-03-06 15:09:59 +0800

    feat: optimize public-path configuration

--- index.js
+++ index.js
@@ -1,27 +1,18 @@
-module.exports = (api, projectOptions) => {
-  const { name, scripts } = api.service.pkg
-  const { publicPath, devServer } = projectOptions
+const path = require('path')
 
-  // 自动注入 publicPath 配置
-  if (
-    process.env.NODE_ENV !== 'production' &&
-    publicPath === '/'
-  ) {
-    // eslint-disable-next-line camelcase
-    const { npm_lifecycle_event, PORT } = process.env
-
-    const currentScript = scripts[npm_lifecycle_event] || ''
-
-    const { port } = require('minimist')(currentScript.split(' '))
-
-    const realPort = port || PORT || devServer.port
-
-    if (realPort) {
-      projectOptions.publicPath = `//localhost:${realPort}/`
-    }
-  }
+module.exports = (api) => {
+  const { name } = api.service.pkg
 
   api.chainWebpack((config) => {
+    config.merge({
+      entry: {
+        main: [
+          path.resolve(__dirname, './public-path.js'),
+          path.resolve(api.service.context, 'src/main.js')
+        ]
+      }
+    })
+
     config.devServer
       .headers({
         'Access-Control-Allow-Origin': '*'
